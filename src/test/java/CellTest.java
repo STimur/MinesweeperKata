1af89ae5ba4cbd2972e690f50cf7134f687a3889
Timur <timur.salakhetdinov@gmail.com>
2017-11-03 04:27:59 +0300

    init commit

---
+++ src/test/java/CellTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CellTest {
+    @Test
+    public void nothing() throws Exception {
+        assertEquals(2, 1+1);
+    }
+}
